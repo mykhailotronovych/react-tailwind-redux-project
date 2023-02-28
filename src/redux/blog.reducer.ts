33925472d9bcdd9bc0723153819fcccd471da2a8
mykhailotronovych <mykhailotronovych@gmail.com>
2023-02-28 19:37:23 +0700

    using create slice

--- src/redux/blog.reducer.ts
+++ src/redux/blog.reducer.ts
@@ -1,6 +1,7 @@
 import { Post } from '../types';
-import { createReducer, createAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { initialPostList } from '../constants';
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 interface BlogState {
     postList: Post[];
@@ -12,30 +13,71 @@ const initialState: BlogState = {
     editPost: null,
 };
 
-// post action
-export const addPost = createAction<Post>('blog/ADD_POST');
-export const deletePost = createAction<string>('blog/DELETE_POST');
-export const startEdit = createAction<string>('blog/START_EDIT');
-export const cancelEdit = createAction('blog/CANCEL_EDIT');
-export const editingPost = createAction<Post>('blog/EDIT_POST');
+// ! this is old way
+// * post action
+// export const addPost = createAction<Post>('blog/ADD_POST');
+// export const deletePost = createAction<string>('blog/DELETE_POST');
+// export const startEdit = createAction<string>('blog/START_EDIT');
+// export const cancelEdit = createAction('blog/CANCEL_EDIT');
+// export const editingPost = createAction<Post>('blog/EDIT_POST');
 
-// post reducer
-const blogReducer = createReducer(initialState, (builder) => {
-    builder
-        .addCase(addPost, (state, action) => {
+// * post reducer
+// const blogReducer = createReducer(initialState, (builder) => {
+//     builder
+//         .addCase(addPost, (state, action) => {
+//             state.postList.push(action.payload);
+//         })
+//         .addCase(deletePost, (state, action) => {
+//             const postId = action.payload;
+//             state.postList = state.postList.filter((post) => post.id !== postId);
+//         })
+//         .addCase(startEdit, (state, { payload: postId }) => {
+//             const foundedPost = state.postList.find((post) => post.id === postId);
+//             if (foundedPost) {
+//                 state.editPost = foundedPost;
+//             }
+//         })
+//         .addCase(editingPost, (state, action) => {
+//             const { id } = action.payload;
+//             state.postList.some((post, index) => {
+//                 if (post.id === id) {
+//                     state.postList[index] = action.payload;
+//                     return true;
+//                 }
+//                 return false;
+//             });
+//         })
+//         .addCase(cancelEdit, (state, action) => {
+//             state.editPost = null;
+//         });
+// });
+
+// export default blogReducer;
+
+// ! this is new way
+// * createSlice là sự kết hợp của createReducer và createAction
+// * createSlice sẽ tự động tạo ra action và reducer
+const blogReducer = createSlice({
+    name: 'blog', // * Đây là prefix cho action type
+    initialState,
+    reducers: {
+        addPost: (state, action: PayloadAction<Post>) => {
             state.postList.push(action.payload);
-        })
-        .addCase(deletePost, (state, action) => {
+        },
+        deletePost: (state, action: PayloadAction<string>) => {
             const postId = action.payload;
             state.postList = state.postList.filter((post) => post.id !== postId);
-        })
-        .addCase(startEdit, (state, { payload: postId }) => {
+        },
+        startEdit: (state, { payload: postId }: PayloadAction<string>) => {
             const foundedPost = state.postList.find((post) => post.id === postId);
             if (foundedPost) {
                 state.editPost = foundedPost;
             }
-        })
-        .addCase(editingPost, (state, action) => {
+        },
+        cancelEdit: (state) => {
+            state.editPost = null;
+        },
+        editingPost: (state, action: PayloadAction<Post>) => {
             const { id } = action.payload;
             state.postList.some((post, index) => {
                 if (post.id === id) {
@@ -44,10 +86,9 @@ const blogReducer = createReducer(initialState, (builder) => {
                 }
                 return false;
             });
-        })
-        .addCase(cancelEdit, (state, action) => {
-            state.editPost = null;
-        });
+        },
+    },
 });
 
-export default blogReducer;
+export const { addPost, cancelEdit, deletePost, editingPost, startEdit } = blogReducer.actions;
+export default blogReducer.reducer;
